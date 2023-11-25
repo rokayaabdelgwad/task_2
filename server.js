ed84d585f68cb5260463df73442189da59dedda5
Rokaya abdelgwad <rokayaabdelgwad246@gmail.com>
2023-11-25 19:57:32 +0200

    commit

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 require("./db");
 const app = require("./app");
-const socket = require("socket.io");
+
 
 
 const port = process.env.PORT || 3001 || 3002;
 
-const server =app.listen(port, () => {
+app.listen(port, () => {
   console.log(`listening on ${port} 👍👍`);
 });
 
-// Socket setup
-const io = socket(server);
 
-io.on("connection", function (socket) {
-  console.log("Made socket connection");
-});
